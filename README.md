e86ff756a97e7406ead08f14cc195a6e18385331
LeafLight <841789075@qq.com>
2022-02-09 13:18:23 +0800

    v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Blog-Note-Vim
 Some simple scripts aim to make blog synchronization and notes management easier.
+
+# Usage
+1. blogSync: a script which makes blog sychronization steps of hexo easier
+2. doctree: just a simple script containing 'tree ~/Documents'
+3. notevim: a shell script to manage notes
+# Get Started
+1. Put these file in the same filefold(use `~/bin/` as example).
+2. Add commands below to your `~/.profile`(change the path if needed)
+```bash
+export PATH=$PATH:~/bin/blogSync 
+export PATH=$PATH:~/bin/doctree
+export PATH=$PATH:~/bin/notevim
+```
+3. To use `doctree`, just execute the command `doctree`
+4. To use `blogSync`, `cd` to the hexo blog filefold and just execute the command `blogSync`
+5. To use `notevim`, which is the most complex one in this repository though it is simple for many 'linuxers', there are more steps:
+	1. Add the note name and its dir path to the `notevimFileDir.json`. Here is an example.
+		```json
+		{
+			"notename1":"path1",
+			"notename2":"path2"
+		}
+		```
+	2. Now you can edit the note by vim using `notevim -n notename1` instead of typing the full command of vim, which requires the full dir path. And use `notevim -l` to show all the notes defined in `notevimFileDir`.
+	3. There is an additional feature for hexo blog(or you can make it custom by edit the source codes, which are annotated in detail).Use `notevim -u notename1` to synchronize the note1 in the original place and note1 in the hexo filefold,which will remove the older one and copy the other one to corresponding place.Before Using it, you need to add the hexo filefold path to the `notevimFileDir.json`, using the name `BlogDir`:
+		```json
+		{
+			"BlogDir":"path"
+		}
+		```
+# Postscript
+
+They are just some exercise scripts used to practice the varibles sharement between scripts, may the fun be with you.
---
+++ notevim_utils.py
@@ -0,0 +1,103 @@
+#notevim_utils.py
+########################################
+#import
+import json
+import sys
+import getopt
+import os
+import time
+from os.path import expanduser
+########################################
+#utils_func
+# get the note dir of the name defined by '-n'
+# 'BlogDir' is reserved for the blog Directory of Hexo
+def GetTheDir(filename=None):
+    #skip the first arg. : the name of this script
+    with open("notevimFileDir.json", 'r') as f:
+        DirDict = json.load(f)
+    return DirDict[filename]
+# list all the note and their dirs
+def ListTheNote():
+    with open("notevimFileDir.json", 'r') as f:
+        DirDict = json.load(f)
+        print("All of the notes and their directories:")
+        print("________________________________________")
+        for key,value in DirDict.items():
+            print(key,value)
+# replace ~ in filePath because python don't use it as home dir in default
+def ReplaceTildeInDir(filePath):
+    if filePath[0] == '~':
+        filePath = filePath.replace('~',expanduser("~"))
+    return filePath
+# get the file name of the note by splitting the file path
+def get_filename(filePath):
+    return filePath.split('/')[-1]
+# convert time stamp into time in format defined by time.asctime()
+def TimeStampToTime(timestamp):
+    return time.asctime(time.localtime(timestamp))
+# get the modification time of the file referred
+def get_ModifyTime(filePath, convert=False):
+    mtime = os.path.getmtime(ReplaceTildeInDir(filePath))
+    if convert :
+        mtime = TimeStampToTime(mtime)
+    return mtime
+# get the corresponding blog path of the note by spliting and catenating
+def get_BlogDir(filePath):
+    return GetTheDir("BlogDir")+get_filename(filePath)
+    #list the names of the code
+########################################
+#main
+argv = sys.argv[1:]
+try:
+    #short options mode
+    opts, args = getopt.getopt(argv,'n::lc:u:')
+except:
+    print('Error: Invalid option(s), from notevim_utils.py')
+
+for opt,val in opts:
+    if opt in ['-n']:
+        #name of the note
+        filename = val
+        #get the directory of the note and edit it
+        os.system('vim %s'%(GetTheDir(filename))) 
+    if opt in ['-l']:
+        #list the names of the note
+        ListTheNote()
+    if opt in ['-c']:
+        #copy the note into Blog dir
+        filename = val
+        cmd = 'cp %s %s'%(GetTheDir(filename), get_BlogDir(GetTheDir(filename))) 
+        os.system('cp %s %s'%(GetTheDir(filename), get_BlogDir(GetTheDir(filename))))
+        print("command(s) executed:")
+        print("________________________________________")
+        print(cmd)
+    if opt in ['-u']:
+        #update the specifc file(rm the old one and cp the new one)
+        filename = val
+        filePath = GetTheDir(filename)
+        #get two modification time stamps respectively
+        raw_file_mtime = get_ModifyTime(filePath, convert=True)
+        gbs_file_mtime = get_ModifyTime(get_BlogDir(filePath), convert=True)
+        print("raw file modification time: ",raw_file_mtime)
+        print("blg file modification time: ",gbs_file_mtime)
+        print("________________________________________")
+        print("Update the old one?[Y/n]")
+        while True:
+            Yn = input()
+            if (Yn == 'y')|(Yn == 'Y'):
+                if raw_file_mtime<gbs_file_mtime:
+                    os.system('rm %s'%(filePath))
+                    os.system('cp %s %s'%(get_BlogDir(GetTheDir(filename)), GetTheDir(filename)))
+                    break
+                else:
+                    os.system('rm %s'%(get_BlogDir(filePath)))
+                    os.system('cp %s %s'%(GetTheDir(filename), get_BlogDir(GetTheDir(filename))))
+                    break
+
+            elif (Yn == 'n')|(Yn =='N'):
+                break
+            else:
+                print("Invalid input,Try again...[Y/n]")
+
+                
+
